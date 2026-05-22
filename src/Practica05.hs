b8b5c945ab2852dd8bfa92d0d81b491175d3c748
Roman Contreras <roman.contreras@ciencias.unam.mx>
2026-05-21 18:44:16 -0600

    feat: apsubT implementada

--- src/Practica05.hs
+++ src/Practica05.hs
@@ -2,32 +2,37 @@ module Practica05 where
 
 import Terminos
 
---Aplicar una sustitucion a un termino
+buscaNombre :: Nombre -> Subst -> Maybe Term
+buscaNombre nombre [] = Nothing
+buscaNombre nombre ((x, t) : rest) = if nombre == x then Just t else buscaNombre nombre rest
+
+-- Aplicar una sustitucion a un termino
 apsubT :: Term -> Subst -> Term
-apsubT = undefined 
+apsubT (Var x) sbsts = case buscaNombre x sbsts of
+  Just t -> t
+  Nothing -> Var x
+apsubT (Fun f args) sbsts = Fun f (aplicarLista args sbsts)
 
---Funcion auxiliar para aplicar la sustitucion a una lista de terminos
+-- Funcion auxiliar para aplicar la sustitucion a una lista de terminos
 aplicarLista :: [Term] -> Subst -> [Term]
-aplicarLista = undefined 
+aplicarLista ts sbsts = map (\t -> apsubT t sbsts) ts
 
---Funcion que elimina los pares que son de la forma x=x
+-- Funcion que elimina los pares que son de la forma x=x
 simpSus :: Subst -> Subst
 simpSus = undefined
 
---Funcion que calcula la composicion de dos sustituciones
+-- Funcion que calcula la composicion de dos sustituciones
 compSus :: Subst -> Subst -> Subst
 compSus = undefined
 
---Funcion que devuelve un umg de dos terminos, si es que lo hay
+-- Funcion que devuelve un umg de dos terminos, si es que lo hay
 unifica :: Term -> Term -> [Subst]
 unifica = undefined
 
-
---Funcion que devuelve un unificador de dos términos funcionales, si es que lo hay
+-- Funcion que devuelve un unificador de dos términos funcionales, si es que lo hay
 unificaListas :: [Term] -> [Term] -> [Subst]
 unificaListas = undefined
 
---Funcion que devuelve un umg de una lista de termino, si es que lo hay
+-- Funcion que devuelve un umg de una lista de termino, si es que lo hay
 unificaConj :: [Term] -> [Subst]
 unificaConj = undefined
-
